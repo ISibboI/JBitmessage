61032c875dc7a2a085c1b21393dc9be0b1adb9b5
Sebastian Schmidt <isibboi@gmail.com>
2013-04-06 13:58:30 +0200

    Created emtpy Connection class

---
+++ src/sibbo/bitmessage/network/Connection.java
@@ -0,0 +1,14 @@
+package sibbo.bitmessage.network;
+
+import java.util.logging.Logger;
+
+public class Connection implements Runnable {
+	private static final Logger LOG = Logger.getLogger(Connection.class
+			.getName());
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
