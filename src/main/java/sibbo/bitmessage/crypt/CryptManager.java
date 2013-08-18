06a58d1a0724a53717b960efd3512f29cbd21208
Sebastian Schmidt <isibboi@gmail.com>
2013-08-18 21:19:27 +0200

    Started to implement the encryption

--- src/main/java/sibbo/bitmessage/crypt/CryptManager.java
+++ src/main/java/sibbo/bitmessage/crypt/CryptManager.java
@@ -2,37 +2,38 @@ package sibbo.bitmessage.crypt;
 
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.security.Security;
-import java.security.spec.ECGenParameterSpec;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-
-import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.DataLengthException;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.BlockCipherPadding;
+import org.bouncycastle.crypto.paddings.PKCS7Padding;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.JCEECPrivateKey;
+import org.bouncycastle.jce.provider.JCEECPublicKey;
 import org.bouncycastle.jce.spec.ECParameterSpec;
-import org.bouncycastle.jce.spec.IEKeySpec;
-import org.bouncycastle.jce.spec.IESParameterSpec;
-import org.bouncycastle.math.ec.ECCurve;
+import org.bouncycastle.math.ec.ECPoint;
 
 import sibbo.bitmessage.Options;
-import sibbo.bitmessage.crypt.Digest;
-import sibbo.bitmessage.crypt.KeyDataPair;
-import sibbo.bitmessage.crypt.POWCalculator;
 import sibbo.bitmessage.network.protocol.Util;
 
 public final class CryptManager {
-	private static final Logger LOG = Logger.getLogger(CryptManager.class
-			.getName());
+	private static final Logger LOG = Logger.getLogger(CryptManager.class.getName());
 
 	public static CryptManager instance;
 
@@ -52,19 +53,19 @@ public final class CryptManager {
 	}
 
 	private KeyPairGenerator kpg;
-	private IESParameterSpec iesps;
+	private ECParameterSpec ecGenSpec;
 
 	public boolean initialize() {
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-		
+
 		try {
 			kpg = KeyPairGenerator.getInstance("ECIES", "BC");
-		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+			ecGenSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
+			kpg.initialize(ecGenSpec, new SecureRandom());
+		} catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidAlgorithmParameterException e) {
 			LOG.log(Level.SEVERE, "No ECIES cryptography available!", e);
 			return false;
 		}
-		
-		iesps = new IESParameterSpec(new byte[]{1, 2, 3, 4, 5, 6, 7, 8}, new byte[]{8, 7, 6, 5, 4, 3, 2, 1}, 128);
 
 		return true;
 	}
@@ -98,30 +99,8 @@ public final class CryptManager {
 	 *         with the given key.
 	 */
 	public KeyDataPair tryDecryption(KeyDataPair encrypted) {
-		Cipher cipher = null;
-
-			try {
-				cipher = Cipher.getInstance("ECIES", "BC");
-			} catch (NoSuchAlgorithmException | NoSuchProviderException
-					| NoSuchPaddingException e) {
-				LOG.log(Level.SEVERE, "No ECIES cryptography available!", e);
-				return null;
-			}
-
-		try {
-			cipher.init(Cipher.DECRYPT_MODE, new IEKeySpec(encrypted.getKey()
-					.getPrivate(), encrypted.getKey().getPublic()), iesps);
-		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
-			LOG.log(Level.SEVERE, "Invalid Key", e);
-			return null;
-		}
-
-		try {
-			return new KeyDataPair(encrypted.getKey(), cipher.doFinal(encrypted.getData()));
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			LOG.log(Level.SEVERE, "Could not encrypt message.", e);
-			return null;
-		}
+		// TODO
+		return null;
 	}
 
 	/**
@@ -132,32 +111,120 @@ public final class CryptManager {
 	 * @return The data encrypted with the given key.
 	 */
 	public KeyDataPair encrypt(KeyDataPair plain) {
-		Cipher cipher = null;
+		KeyPair random = null;
 
 		synchronized (kpg) {
-			try {
-				cipher = Cipher.getInstance("ECIES", "BC");
-			} catch (NoSuchAlgorithmException | NoSuchProviderException
-					| NoSuchPaddingException e) {
-				LOG.log(Level.SEVERE, "No ECIES cryptography available!", e);
-				return null;
-			}
+			random = kpg.generateKeyPair();
 		}
 
+		ECPoint point = ((JCEECPublicKey) plain.getKey().getPublic()).getQ().multiply(
+				((JCEECPrivateKey) random.getPrivate()).getD());
+		byte[] tmpKey = deriveKey(point);
+		byte[] key_e = Arrays.copyOfRange(tmpKey, 0, 32);
+		byte[] key_m = Arrays.copyOfRange(tmpKey, 32, 64);
+		byte[] iv = new byte[16];
+		new SecureRandom().nextBytes(iv);
+
+		byte[] encrypted = doAES(key_e, iv, plain.getData(), true);
+		byte[] mac = Digest.hmacSHA256(encrypted, key_m);
+
+		byte[] result = new byte[16 + 70 + encrypted.length + 32];
+		System.arraycopy(iv, 0, result, 0, 16);
+		System.arraycopy(new byte[] { 0x02, (byte) 0xCA }, 0, result, 16, 2);
+		System.arraycopy(new byte[] { 0x00, 0x20 }, 0, result, 18, 2);
+		System.arraycopy(getBytes(point.getX().toBigInteger(), 32), 0, result, 20, 32);
+		System.arraycopy(new byte[] { 0x00, 0x20 }, 0, result, 52, 2);
+		System.arraycopy(getBytes(point.getY().toBigInteger(), 32), 0, result, 54, 32);
+		System.arraycopy(encrypted, 0, result, 16 + 70, encrypted.length);
+		System.arraycopy(mac, 0, result, 16 + 70 + encrypted.length, 32);
+
+		return new KeyDataPair(plain.getKey(), result);
+	}
+
+	/**
+	 * En- or decrypts the given data with the given key.
+	 * 
+	 * @param keyBytes
+	 *            The AES key.
+	 * @param data
+	 *            The data to process.
+	 * @param encrypt
+	 *            True if the data should be encrypted, false if it should be
+	 *            decrypted.
+	 * @return The en- or decrypted data.
+	 */
+	private byte[] doAES(byte[] keyBytes, byte[] iv, byte[] data, boolean encrypt) {
+		BlockCipherPadding padding = new PKCS7Padding();
+		BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESEngine()), padding);
+
+		KeyParameter key = new KeyParameter(keyBytes);
+		CipherParameters params = new ParametersWithIV(key, iv);
+
+		cipher.init(encrypt, params);
+
+		byte[] buffer = new byte[cipher.getOutputSize(data.length)];
+		int length = cipher.processBytes(data, 0, data.length, buffer, 0);
+
 		try {
-			cipher.init(Cipher.ENCRYPT_MODE, new IEKeySpec(plain.getKey()
-					.getPrivate(), plain.getKey().getPublic()), iesps);
-		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
-			LOG.log(Level.SEVERE, "Invalid Key", e);
+			length += cipher.doFinal(buffer, length);
+		} catch (DataLengthException | IllegalStateException | InvalidCipherTextException e) {
+			LOG.log(Level.SEVERE, "Could not execute AES.", e);
 			return null;
 		}
 
-		try {
-			return new KeyDataPair(plain.getKey(), cipher.doFinal(plain.getData()));
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			LOG.log(Level.SEVERE, "Could not encrypt message.", e);
-			return null;
+		return Arrays.copyOf(buffer, length);
+	}
+
+	/**
+	 * Returns a positive BigInteger from the given bytes. (Big endian)
+	 * 
+	 * @param data
+	 *            The bytes.
+	 * @param offset
+	 *            The first byte.
+	 * @param length
+	 *            The amount of bytes to process.
+	 * @return A BigInteger from the given bytes.
+	 */
+	private BigInteger getUnsignedBigInteger(byte[] data, int offset, int length) {
+		byte[] value = new byte[length + 1];
+		System.arraycopy(data, offset, value, 1, length);
+
+		return new BigInteger(value);
+	}
+
+	/**
+	 * Returns a byte[] representation of the given big integer, cutting it to
+	 * the given length.
+	 * 
+	 * @param number
+	 *            The BigInteger.
+	 * @param length
+	 *            The maximum length.
+	 * @return The last <code>length</code> bytes of the given big integer,
+	 *         filled with zeros if necessary.
+	 */
+	private byte[] getBytes(BigInteger number, int length) {
+		byte[] value = number.toByteArray();
+		byte[] result = new byte[length];
+
+		int i = value.length == length + 1 ? 1 : 0;
+		for (; i < value.length; i++) {
+			result[i + length - value.length] = value[i];
 		}
+
+		return result;
+	}
+
+	/**
+	 * Derives a 512 bit key from the given ECPoint.
+	 * 
+	 * @param p
+	 *            An ECPoint.
+	 * @return A 512 bit key.
+	 */
+	private byte[] deriveKey(ECPoint p) {
+		return Digest.sha512(getBytes(p.getX().toBigInteger(), 32));
 	}
 
 	/**
@@ -191,12 +258,9 @@ public final class CryptManager {
 
 		BigInteger powTarget = BigInteger.valueOf(2);
 		powTarget = powTarget.pow(64);
-		powTarget = powTarget.divide(BigInteger
-				.valueOf((length
-						+ Options.getInstance().getInt(
-								"pow.payloadLengthExtraBytes") + 8)
-						* Options.getInstance().getInt(
-								"pow.averageNonceTrialsPerByte")));
+		powTarget = powTarget.divide(BigInteger.valueOf((length
+				+ Options.getInstance().getInt("pow.payloadLengthExtraBytes") + 8)
+				* Options.getInstance().getInt("pow.averageNonceTrialsPerByte")));
 
 		// Note that we are dividing through at least 8, so that the value is
 		// smaller than 2^61 and fits perfectly into a long.
@@ -211,19 +275,32 @@ public final class CryptManager {
 	 * @return
 	 */
 	public byte[] doPOW(byte[] payload) {
-		POWCalculator pow = new POWCalculator(getPOWTarget(payload.length),
-				Digest.sha512(payload), Options.getInstance().getInt(
-						"pow.systemLoad"));
+		POWCalculator pow = new POWCalculator(getPOWTarget(payload.length), Digest.sha512(payload), Options
+				.getInstance().getInt("pow.systemLoad"));
 		return pow.execute();
 	}
 
 	/**
-	 * Creates a KeyPair containing only the given private key. The value of the public key will be undefined.
-	 * @param privateEncryptionKey The private encryption key.
+	 * Creates a KeyPair containing only the given private key. The value of the
+	 * public key will be undefined.
+	 * 
+	 * @param privateEncryptionKey
+	 *            The private encryption key.
 	 * @return A KeyPair containing only the given private key.
 	 */
 	public KeyPair createKeyPairWithPrivateKey(byte[] privateEncryptionKey) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * Generates a new random ECIES key pair.
+	 * 
+	 * @return A new random ECIES key pair.
+	 */
+	public KeyPair generateKeyPair() {
+		synchronized (kpg) {
+			return kpg.generateKeyPair();
+		}
 	}
 }
